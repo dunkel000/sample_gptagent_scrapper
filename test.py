63476aa5ead00fffcb29f09f37e8fe28ad156afc
Diego-Ignacio Ortiz <31400790+dunkel000@users.noreply.github.com>
2025-07-28 19:48:59 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=webdriver.ChromeOptions()
+)
+print("Tamaño de ventana:", driver.get_window_size())
+
+
+driver.get('https://google.com')
+print(driver.title)
+driver.quit()
+
+
